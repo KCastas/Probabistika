3d11d518b2ff0f241ccfbc6865731bd7694dd6f6
KCastas <kchantsaris@hotmail.com>
2025-12-11 13:48:22 +0200

    fixed, working mean & median

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <numeric> 
 #include <algorithm>
@@ -11,7 +12,7 @@ public:
 	Dataset(const std::vector<double>& data) : observations(data) {}
 
 	double calculateMean(){
-		double sum = std::accumulate(observations.begin(), observations.end(), 0,0);
+		double sum = std::accumulate(observations.begin(), observations.end(), 0.0);
 
 		int n = observations.size();
 
@@ -26,7 +27,8 @@ public:
 	}
 	double calculateMedian(){
 		int n = observations.size();
-
+		
+		std::sort(observations.begin(), observations.end());
 		
 		if (n % 2 != 0) {
 			median = observations[n/2];
@@ -36,11 +38,11 @@ public:
 			return 0.0;
 		}
 		
-		std::sort(observations.begin(), observations.end());
-		int lowerindex = (int)n / 2;
+
+		int lowerIndex = (int)n / 2;
 		
-		lowerHalfObservation = observations[lowerIndex];
-		higherHalfObservation = observations[lowerIndex + 1];
+		double lowerHalfObservation = observations[lowerIndex -1];
+		double higherHalfObservation = observations[lowerIndex];
 		
 		median = (lowerHalfObservation + higherHalfObservation)/2.0;
 
@@ -48,3 +50,13 @@ public:
 
 	}
 };
+
+
+int main (){
+	Dataset dataset({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+
+	std::cout << "Calculated Mean: " << dataset.calculateMean() << "\n";
+	std::cout << "Calculated Median: " << dataset.calculateMedian() << "\n";
+
+	return 0;
+}
